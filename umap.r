24d76e8f82624d55e7880b7d20e71cff41127f50
fmoore <j.desbiens@gmail.com>
2018-07-21 17:28:21 -0500

    Restored Umap code correctly.

--- umap.r
+++ umap.r
@@ -1,6 +1,10 @@
-data(iris)
-res <- umap(iris[,1:4])
-tibble(x = res$layout[,1],y = resres$layout[,2],species = iris$Species) %>% 
-  ggplot(aes(x = x,y = y,col = species))+
+#library("ggplot2", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+#library("umap", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+#library("magrittr", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+#library("tibble", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.2")
+
+res <- umap(ts[,1:3])
+tibble(x = res$layout[,1], y = res$layout[,2], Severity = ts$Severity) %>% 
+  ggplot(aes(x = x,y = y,col = Severity))+
   geom_point()+
   theme(legend.position = "bottom")
